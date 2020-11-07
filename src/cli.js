14e5bad690d640f0219be4b0f85187e2d2fda616
Joaquin Gabriel Veliz <joaquingabriel.veliz@naranja.com>
2020-11-07 13:13:11 -0300

    Render an entire directory passed as a parameter

--- src/cli.js
+++ src/cli.js
@@ -1,23 +1,37 @@
+import { throws } from "assert";
+import { exception } from "console";
+import { chdir } from "process";
+
 const glob = require("glob")
 const Handlebars = require("handlebars");
 const fs   = require('fs');
 const path = require('path')
 export function cli(args) {
-    glob("**/*.{conf,yml}",{ignore: "node_modules/**/*.*"}, function (er, files) {
-      for (let index = 0; index < files.length; index++) {
-        const element = files[index];
-        console.log(element);
-        const fullPath = path.resolve(element)
-        // Rename template to a temp file
-        fs.renameSync(fullPath, fullPath + ".temp")
-        const envFile = require("./" + process.argv[2])
-        const templateFile = fs.readFileSync("./" + element + ".temp", 'utf8');
-        
-        const template = Handlebars.compile(templateFile);
-        const parsedTemplate = template(envFile);
-        console.log(parsedTemplate);
-        fs.writeFile(fullPath, parsedTemplate, err =>{})
-        fs.unlink(fullPath + ".temp", (err) => {})
-      }
-    })
+  const envFile = require("./" + args[2])
+  chdir(args[3])
+  glob("**/*.{conf,yml,js,yaml}",{ignore: "node_modules/**/*.*"}, function (er, files) {
+    for (let index = 0; index < files.length; index++) {
+      const element = files[index];
+      console.log(element);
+      // Get file absolute path
+      const fullPath = path.resolve(element);
+      // Rename template to a temp file
+      fs.renameSync(fullPath, fullPath + ".temp");
+      // Read temp file
+      const templateFile = fs.readFileSync("./" + element + ".temp", 'utf8');
+      const template = Handlebars.compile(templateFile);
+      // Render template
+      const renderedTemplate = template(envFile);
+      fs.writeFile(fullPath, renderedTemplate, err =>{
+        if(err){
+          throw exception(err.message)
+        }
+      })
+      fs.unlink(fullPath + ".temp", (err) => {
+        if(err){
+          throw exception(err.message)
+        }
+      });
+    }
+  })
 }
