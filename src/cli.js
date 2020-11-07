3cf78a237ae64465a9a8282d33bd5f0d4fc3cdb8
Joaquin Gabriel Veliz <joaquingabriel.veliz@naranja.com>
2020-11-07 13:40:38 -0300

    fixed error getting file paths

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,4 @@
-import { throws } from "assert";
-import { exception } from "console";
+
 import { chdir } from "process";
 
 const glob = require("glob")
@@ -7,7 +6,7 @@ const Handlebars = require("handlebars");
 const fs   = require('fs');
 const path = require('path')
 export function cli(args) {
-  const envFile = require("./" + args[2])
+  const envFile = require( process.cwd() + "/" + args[2])
   chdir(args[3])
   glob("**/*.{conf,yml,js,yaml}",{ignore: "node_modules/**/*.*"}, function (er, files) {
     for (let index = 0; index < files.length; index++) {
@@ -18,18 +17,18 @@ export function cli(args) {
       // Rename template to a temp file
       fs.renameSync(fullPath, fullPath + ".temp");
       // Read temp file
-      const templateFile = fs.readFileSync("./" + element + ".temp", 'utf8');
+      const templateFile = fs.readFileSync(process.cwd() + "/" + element + ".temp", 'utf8');
       const template = Handlebars.compile(templateFile);
       // Render template
       const renderedTemplate = template(envFile);
       fs.writeFile(fullPath, renderedTemplate, err =>{
         if(err){
-          throw exception(err.message)
+          throw err
         }
       })
       fs.unlink(fullPath + ".temp", (err) => {
         if(err){
-          throw exception(err.message)
+          throw err
         }
       });
     }
