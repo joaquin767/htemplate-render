c521d04a2a620753c26c9624aaf30bd057ef0c9c
Joaquin Gabriel Veliz <joaquingabriel.veliz@naranja.com>
2020-11-15 01:17:41 -0300

    restructured using @ocliff library

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# template-parser
+template-render
+===============
+
+A template parser-render for multi purposes
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/template-render.svg)](https://npmjs.org/package/template-render)
+[![Downloads/week](https://img.shields.io/npm/dw/template-render.svg)](https://npmjs.org/package/template-render)
+[![License](https://img.shields.io/npm/l/template-render.svg)](https://github.com/joaquin767/template-parser/template-render/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+# Commands
+<!-- commands -->
---
+++ src/commands/render.js
@@ -0,0 +1,61 @@
+const {Command, flags} = require('@oclif/command')
+const path = require('path')
+const fs = require('fs')
+const glob = require("glob")
+const Handlebars = require("handlebars");
+
+class RenderCommand extends Command {
+  async run() {
+    const {flags} = this.parse(RenderCommand)
+    if (!flags.dir || !flags.envFile) {
+      this._help();
+    }
+
+    const directory = path.resolve(flags.dir)
+    const environmentFile = require(path.resolve(flags.envFile))
+    process.chdir(directory)
+    this.log(process.cwd())
+    glob("**/*.{conf,yml,js,yaml}", { ignore: "node_modules/**/*.*" }, function (er, files) {
+      for (let index = 0; index < files.length; index++) {
+        const element = files[index];
+        this.log();
+        // Get file absolute path
+        const fullPath = path.resolve(element);
+        // Rename template to a temp file
+        fs.renameSync(fullPath, fullPath + ".temp");
+        // Read temp file
+        const templateFile = fs.readFileSync(process.cwd() + "/" + element + ".temp", 'utf8');
+        const template = Handlebars.compile(templateFile);
+        // Render template
+        const renderedTemplate = template(environmentFile);
+        fs.writeFile(fullPath, renderedTemplate, err => {
+          if (err) {
+            throw err
+          }
+        })
+        fs.unlink(fullPath + ".temp", (err) => {
+          if (err) {
+            throw err
+          }
+        });
+      }
+    })
+  }
+}
+
+RenderCommand.description = `Render an entire directory
+...
+Provide the relative directory path with -d
+Also provide a file with all the environments variables in JSON format with -e
+The entire directory will be scanned and all the templates  will be renderized if is necessary
+By default all the templates will be modified. Use the flag --dry-run to only renderize but not update
+the files.
+`
+
+RenderCommand.flags = {
+  dir: flags.string({char: 'd', description: '(Required) directory to render'}),
+  envFile: flags.string({char: 'e', description: '(Required) environment file in JSON format'}),
+  'dry-run': flags.boolean({description: 'render only and avoid the modification of the templates'})
+}
+
+module.exports = RenderCommand
